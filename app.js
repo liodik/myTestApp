8ffb3bd947c3a7e8363c9e3a3e672e08f777ace2
volodymyrm <volo>
2023-02-05 12:24:57 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const compression = require("compression");
+
+const authRoutes = require("./routes/auth");
+
+const app = express();
+app.use(helmet());
+app.use(compression());
+
+
+// app.use(bodyParser.urlencoded());  // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/auth", authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.status || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.nxloz2t.mongodb.net/${process.env.MONGO_DB}`
+  )
+  .then((res) => app.listen(process.env.PORT || 3000))
+  .catch((err) => console.log(err));
